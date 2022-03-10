eed1861de0b247b2d6005cff905c01847d0c7edc
oscargargom001 <oscargargom@hotmail.com>
2022-03-10 12:44:41 +0100

    Completado archivos

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+use Symfony\Component\BrowserKit\Request;
+
+
+/**
+ * @Route("/eventos", name="eventos")
+ * 
+ */
+public function eventos(Request $request, CommonService $commonService)
+{
+    $authcode = $request->get(key:"authcode");
+    $email = "oscargargom001@gmail.com";
+
+    if(!empty($authcode)) $commonService->insertGoogleApiToken($email,$authcode);
+
+    $authLink = $commonService->generateGoogleApiToken($email);
+    $eventos = $commonService->getEventsByEmail($email);
+
+    return $this->render(view:'default/eventos.html.twig', [
+
+        'eventos' => $eventos,
+        'authLink' => $authLink,
+
+
+    ]);
+
+
+}
+
+?>
--- src/Service/CommonService.php
+++ src/Service/CommonService.php
@@ -1,105 +1,158 @@
 <?php
+
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 /**
  * Copyright 2018 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 // [START calendar_quickstart]
-require __DIR__ . '/vendor/autoload.php';
 
-if (php_sapi_name() != 'cli') {
-    throw new Exception('This application must be run on the command line.');
+class CommonService{
+
+    private $entityManager;
+
+    private $session;
+
+    private $container;
+
+
+public function _construct(EntityManagerInterface $entityManager, SessionInterface $session, ContainerInterface $container)
+{
+    $this->entityManager = $entityManager;
+    $this->session = $session;
+    $this->container = $container;
 }
 
-/**
- * Returns an authorized API client.
- * @return Google_Client the authorized client object
- */
-function getClient()
+public function getEventsByEmail($email) : array
 {
-    $client = new Google_Client();
-    $client->setApplicationName('Google Calendar API PHP Quickstart');
-    $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
-    $client->setAuthConfig('credentials.json');
-    $client->setAccessType('offline');
-    $client->setPrompt('select_account consent');
+    $client = $this->getClient($email);
+
+    if(!$client) return [];
+
+    $service = new \Google_Service_Calendar($client);
+    $root_dir = $this->container->getParameter(name:'kernel.project_dir') . '/public/';
+
+    // Print the next 10 events on the user's calendar.
+    $calendarId = 'primary';
+    $optParams = array(
+        'maxResults' => 2,
+        'orderBy' => 'startTime',
+        'singleEvents' => true,
+        'timeMin' => date(format:'c'),
+    );
+    $results = $service->events->listEvents($calendarId, $optParams);
+    $events = $results->getItems();
+
+
+    return $events;
+
+}
+
+
+public function getClient($email){
+
+    $root_dir = $this->container->getParameter(name:'kernel.project_dir') . '/public/';
+
+    $client = new \Google_Client();
+    $client->setApplicationName(applicationName:'Google Calendar API PHP Quickstart');
+    $client->setScopes(scope_or_scopes:\Google_Service_Calendar::CALENDAR_READONLY);
+    $client->setAuthConfig(config:'credentials.json');
+    $client->setAccessType(accessType:'offline');
+    $client->setPrompt(setPrompt:'select_account consent');
 
     // Load previously authorized token from a file, if it exists.
     // The file token.json stores the user's access and refresh tokens, and is
     // created automatically when the authorization flow completes for the first
     // time.
-    $tokenPath = 'token.json';
+    $tokenPath = $root_dir-'/tokens/'.$email.'token.json';
     if (file_exists($tokenPath)) {
         $accessToken = json_decode(file_get_contents($tokenPath), true);
         $client->setAccessToken($accessToken);
     }
 
-    // If there is no previous token or it's expired.
+    if($this->checkGoogleApiToken($client, $tokenPath)) return $client;
+
+    return null;
+
+}
+
+
+public function generateGoogleApiToken($email){
+
+    
+    $root_dir = $this->container->getParameter(name:'kernel.project_dir') . '/public/';
+
+    $client = new \Google_Client();
+    $client->setApplicationName(applicationName:'Google Calendar API PHP Quickstart');
+    $client->setScopes(scope_or_scopes:\Google_Service_Calendar::CALENDAR_READONLY);
+    $client->setAuthConfig(config:'credentials.json');
+    $client->setAccessType(accessType:'offline');
+    $client->setPrompt(setPrompt:'select_account consent');
+    $authUrl = '';
+
+    $tokenPath = $root_dir-'/tokens/'.$email.'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }    
+
     if ($client->isAccessTokenExpired()) {
-        // Refresh the token if possible, else fetch a new one.
         if ($client->getRefreshToken()) {
             $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
         } else {
-            // Request authorization from the user.
             $authUrl = $client->createAuthUrl();
-            printf("Open the following link in your browser:\n%s\n", $authUrl);
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
-
-            // Exchange authorization code for an access token.
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            $client->setAccessToken($accessToken);
-
-            // Check to see if there was an error.
-            if (array_key_exists('error', $accessToken)) {
-                throw new Exception(join(', ', $accessToken));
-            }
+            
         }
-        // Save the token to a file.
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        
     }
-    return $client;
+    return $authUrl;
+
+}
+
+public function insertGoogleApiToken($email, $authCode = ""){
+
+if(empty($authCode)) return false;
+
+
+$root_dir = $this->container->getParameter(name:'kernel.project_dir') . '/public/';
+$tokenPath = $root_dir.'/tokens/'.$email.'token.json';
+
+    $client = new \Google_Client();
+    $client->setApplicationName(applicationName:'Google Calendar API PHP Quickstart');
+    $client->setScopes(scope_or_scopes:\Google_Service_Calendar::CALENDAR_READONLY);
+    $client->setAuthConfig(config:'credentials.json');
+    $client->setAccessType(accessType:'offline');
+    $client->setPrompt(setPrompt:'select_account consent');
+    
+    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+    $client->setAccessToken($accessToken);
+
+    if (array_key_exists('error', $accessToken)) {
+
+        return false;
+    }
+
+    if (!file_exists(dirname($tokenPath))) {
+        mkdir(dirname($tokenPath), 0700, true);
+    }
+    file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+
+    return true;
+}
+
 }
 
 
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Calendar($client);
 
-// Print the next 10 events on the user's calendar.
-$calendarId = 'primary';
-$optParams = array(
-  'maxResults' => 10,
-  'orderBy' => 'startTime',
-  'singleEvents' => true,
-  'timeMin' => date('c'),
-);
-$results = $service->events->listEvents($calendarId, $optParams);
-$events = $results->getItems();
 
-if (empty($events)) {
-    print "No upcoming events found.\n";
-} else {
-    print "Upcoming events:\n";
-    foreach ($events as $event) {
-        $start = $event->start->dateTime;
-        if (empty($start)) {
-            $start = $event->start->date;
-        }
-        printf("%s (%s)\n", $event->getSummary(), $start);
-    }
-}
-// [END calendar_quickstart]
+
+
+
+
+
+
+
+
