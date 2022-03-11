39804bb352276c73fd4cc490775ecdddc3847c2d
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-03-11 11:08:02 +0100

    Implementación login (solucionado)

--- migrations/Version20220311095952.php
+++ migrations/Version20220311095952.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220306221829 extends AbstractMigration
+final class Version20220311095952 extends AbstractMigration
 {
     public function getDescription(): string
     {
