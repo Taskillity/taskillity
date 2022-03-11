c87ae30e40820036e03a0c84b700237579fa752b
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-03-11 19:12:49 +0100

    Registro Usuarios Completado

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[ORM\Entity(repositoryClass: UsuarioRepository::class)]
 class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
