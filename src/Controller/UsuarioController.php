b89c7657836cb4f2f3c6547c065f1795a6da4877
pepe <jgomezjimenez@eci.safanet.es>
2022-03-11 12:56:50 +0100

    API REST entidad tarea

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\{TextType, ButtonType, EmailType, HiddenType, PasswordType, TextareaType, SubmitType, NumberType, DateType, MoneyType, BirthdayType};
+
+
+#[Route('/usuario')]
+class UsuarioController extends AbstractController
+{   
+
+
+
+    #[Route('/new', name: 'usuario_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $usuario = new Usuario();
+        $form = $this->createFormBuilder($usuario)
+            ->add('nombre', TextType::class) 
+            ->add('apellidos', TextType::class)
+            ->add('fechaNacimiento', TextType::class)
+            ->add('nacionalidad', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add(
+                'save',
+                SubmitType::class,
+                array('label' => 'Registrar usuario')
+            )
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($usuario);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('usuario/new.html.twig', [
+            'usuario' => $usuario,
+            'form' => $form,
+        ]);
+    }
+
+    
+}
