4571202d09bc1bcfab17c3a4251da8ffc1f5a3fe
pepe <jgomezjimenez@eci.safanet.es>
2022-03-12 12:08:19 +0100

    css index, index.html.twig ...

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -52,7 +52,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         // For example:
         //return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->urlGenerator->generate('index'));
+        return new RedirectResponse($this->urlGenerator->generate('xxxxxxxxx'));
     }
 
     protected function getLoginUrl(Request $request): string
